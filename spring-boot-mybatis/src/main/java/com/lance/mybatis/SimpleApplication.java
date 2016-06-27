73564587c909a400e2110c9bb19fe66ed73c3d94
lance <li6151770@126.com>
2016-06-27 23:15:09 +0800

    update application and SimpleApplication
    update application and SimpleApplication

--- spring-boot-mybatis/src/main/java/com/lance/mybatis/SimpleApplication.java
+++ spring-boot-mybatis/src/main/java/com/lance/mybatis/SimpleApplication.java
@@ -6,10 +6,12 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @SpringBootApplication
 public class SimpleApplication {
@@ -24,7 +26,7 @@ public class SimpleApplication {
 	 * @return
 	 */
 	@Bean
-	public ServletRegistrationBean druidServlet() {
+	public ServletRegistrationBean statViewServlet() {
 		ServletRegistrationBean druid = new ServletRegistrationBean();
 		druid.setServlet(new StatViewServlet());
 		druid.setUrlMappings(Arrays.asList("/druid/*"));
@@ -34,5 +36,18 @@ public class SimpleApplication {
 		params.put("loginPassword", "admin");
 		druid.setInitParameters(params);
 		return druid;
+	}
+	
+	/**
+	 * @see com.alibaba.druid.support.http.WebStatFilter
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean webStatFilter(){
+		FilterRegistrationBean fitler = new FilterRegistrationBean();
+		fitler.setFilter(new WebStatFilter());
+		fitler.setUrlPatterns(Arrays.asList("/*"));
+		fitler.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+		return fitler;
 	}
 }
