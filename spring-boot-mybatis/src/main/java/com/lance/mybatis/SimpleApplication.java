41c757d0b54fce8cc8fdfffa29e4e53e384ed8b5
lance <li6151770@126.com>
2016-06-27 22:39:50 +0800

    add ServletRegistrationBean
    add ServletRegistrationBean

--- spring-boot-mybatis/src/main/java/com/lance/mybatis/SimpleApplication.java
+++ spring-boot-mybatis/src/main/java/com/lance/mybatis/SimpleApplication.java
@@ -1,12 +1,38 @@
 package com.lance.mybatis;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.alibaba.druid.support.http.StatViewServlet;
 
 @SpringBootApplication
 public class SimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
+	}
+	
+	/**
+	 * ServletRegistrationBean,
+	 * @see com.alibaba.druid.support.http.ResourceServlet
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		ServletRegistrationBean druid = new ServletRegistrationBean();
+		druid.setServlet(new StatViewServlet());
+		druid.setUrlMappings(Arrays.asList("/druid/*"));
+		
+		Map<String,String> params = new HashMap<>();
+		params.put("loginUsername", "admin");
+		params.put("loginPassword", "admin");
+		druid.setInitParameters(params);
+		return druid;
 	}
 }
