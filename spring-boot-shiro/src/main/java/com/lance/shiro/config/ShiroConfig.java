3982fd6f7fcc20ddaf07f0e09b93a1e8f9f6b2f8
lance <li6151770@126.com>
2016-06-29 11:40:25 +0800

    update shiro config
    update shiro config

--- spring-boot-shiro/src/main/java/com/lance/shiro/config/ShiroConfig.java
+++ spring-boot-shiro/src/main/java/com/lance/shiro/config/ShiroConfig.java
@@ -1,29 +1,66 @@
 package com.lance.shiro.config;
 
+import java.util.Map;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
 import org.apache.shiro.cache.ehcache.EhCacheManager;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.filter.authc.AnonymousFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
-@Component
+import com.google.common.collect.Maps;
+
+@Configuration
 public class ShiroConfig {
-	@Autowired
-	private UserRealm userRealm;
-
+	
+	/**
+	 * FilterRegistrationBean
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter")); 
+        filterRegistration.setEnabled(true);
+        filterRegistration.addUrlPatterns("/*"); 
+        filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
+        return filterRegistration;
+	}
+	
 	/**
 	 * @see org.apache.shiro.spring.web.ShiroFilterFactoryBean
 	 * @return
 	 */
-	@Bean
-	public ShiroFilterFactoryBean shiroFilterFactoryBean(){
+	@Bean(name = "shiroFilter")
+	public ShiroFilterFactoryBean shiroFilter(){
 		ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
-		bean.setLoginUrl("/login");
 		bean.setSecurityManager(securityManager());
+		bean.setLoginUrl("/login");
+		bean.setUnauthorizedUrl("/unauthorized");
+		
+		Map<String, Filter>filters = Maps.newHashMap();
+		filters.put("perms", urlPermissionsFilter());
+		filters.put("anon", new AnonymousFilter());
+		bean.setFilters(filters);
+		
+		Map<String, String> chains = Maps.newHashMap();
+		chains.put("/login", "anon");
+		chains.put("/unauthorized", "anon");
+		chains.put("/logout", "logout");
+		chains.put("/base/**", "anon");
+		chains.put("/css/**", "anon");
+		chains.put("/layer/**", "anon");
+		chains.put("/**", "authc,perms");
+		bean.setFilterChainDefinitionMap(chains);
 		return bean;
 	}
 	
@@ -32,18 +69,53 @@ public class ShiroConfig {
 	 * @return
 	 */
 	@Bean(name="securityManager")
-	public SecurityManager securityManager() {
+	public DefaultWebSecurityManager securityManager() {
 		DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-		manager.setRealm(userRealm);
-		manager.setCacheManager(new EhCacheManager());
-		manager.setSessionManager(new DefaultWebSessionManager());
+		manager.setRealm(userRealm());
+		//manager.setCacheManager(cacheManager());
+		manager.setSessionManager(defaultWebSessionManager());
 		return manager;
 	}
 	
 	/**
-	 * 保证实现了Shiro内部lifecycle函数的bean执行
+	 * @see DefaultWebSessionManager
 	 * @return
 	 */
+	@Bean(name="sessionManager")
+	public DefaultWebSessionManager defaultWebSessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		//sessionManager.setCacheManager(cacheManager());
+		sessionManager.setGlobalSessionTimeout(1800000);
+		sessionManager.setDeleteInvalidSessions(true);
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		sessionManager.setDeleteInvalidSessions(true);
+		return sessionManager;
+	}
+	
+	/**
+	 * @see UserRealm--->AuthorizingRealm
+	 * @return
+	 */
+	@Bean
+	@DependsOn(value="lifecycleBeanPostProcessor")
+	public UserRealm userRealm() {
+		UserRealm userRealm = new UserRealm();
+		userRealm.setCacheManager(cacheManager());
+		return userRealm;
+	}
+	
+	@Bean
+	public URLPermissionsFilter urlPermissionsFilter() {
+		return new URLPermissionsFilter();
+	}
+	
+	@Bean
+	public EhCacheManager cacheManager() {
+		EhCacheManager cacheManager = new EhCacheManager();
+		cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
+		return cacheManager;
+	}
+	
 	@Bean
 	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
 		return new LifecycleBeanPostProcessor();
