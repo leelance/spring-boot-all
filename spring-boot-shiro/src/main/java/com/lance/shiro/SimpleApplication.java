815d57e23745e298b82f02a019d29d24c4dac189
lance <li6151770@126.com>
2016-06-29 11:41:11 +0800

    update application property
    update application property

--- spring-boot-shiro/src/main/java/com/lance/shiro/SimpleApplication.java
+++ spring-boot-shiro/src/main/java/com/lance/shiro/SimpleApplication.java
@@ -1,9 +1,11 @@
 package com.lance.shiro;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lance.shiro.mapper")
 public class SimpleApplication {
 
 	public static void main(String[] args) {
