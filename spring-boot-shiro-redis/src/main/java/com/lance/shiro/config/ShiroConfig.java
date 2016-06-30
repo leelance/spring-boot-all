4704d1a771eb5be51cf13295f2e5cab00642afa0
lance <li6151770@126.com>
2016-06-30 09:15:24 +0800

    add shiro config
    add shiro config

---
+++ spring-boot-shiro-redis/src/main/java/com/lance/shiro/config/ShiroConfig.java
@@ -0,0 +1,123 @@
+package com.lance.shiro.config;
+
+import java.util.Map;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.filter.authc.AnonymousFilter;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import com.google.common.collect.Maps;
+
+@Configuration
+public class ShiroConfig {
+	
+	/**
+	 * FilterRegistrationBean
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter")); 
+        filterRegistration.setEnabled(true);
+        filterRegistration.addUrlPatterns("/*"); 
+        filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
+        return filterRegistration;
+	}
+	
+	/**
+	 * @see org.apache.shiro.spring.web.ShiroFilterFactoryBean
+	 * @return
+	 */
+	@Bean(name = "shiroFilter")
+	public ShiroFilterFactoryBean shiroFilter(){
+		ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+		bean.setSecurityManager(securityManager());
+		bean.setLoginUrl("/login");
+		bean.setUnauthorizedUrl("/unauthor");
+		
+		Map<String, Filter>filters = Maps.newHashMap();
+		filters.put("perms", urlPermissionsFilter());
+		filters.put("anon", new AnonymousFilter());
+		bean.setFilters(filters);
+		
+		Map<String, String> chains = Maps.newHashMap();
+		chains.put("/login", "anon");
+		chains.put("/unauthor", "anon");
+		chains.put("/logout", "logout");
+		chains.put("/base/**", "anon");
+		chains.put("/css/**", "anon");
+		chains.put("/layer/**", "anon");
+		chains.put("/**", "authc,perms");
+		bean.setFilterChainDefinitionMap(chains);
+		return bean;
+	}
+	
+	/**
+	 * @see org.apache.shiro.mgt.SecurityManager
+	 * @return
+	 */
+	@Bean(name="securityManager")
+	public DefaultWebSecurityManager securityManager() {
+		DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+		manager.setRealm(userRealm());
+		manager.setCacheManager(cacheManager());
+		manager.setSessionManager(defaultWebSessionManager());
+		return manager;
+	}
+	
+	/**
+	 * @see DefaultWebSessionManager
+	 * @return
+	 */
+	@Bean(name="sessionManager")
+	public DefaultWebSessionManager defaultWebSessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		sessionManager.setCacheManager(cacheManager());
+		sessionManager.setGlobalSessionTimeout(1800000);
+		sessionManager.setDeleteInvalidSessions(true);
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		sessionManager.setDeleteInvalidSessions(true);
+		return sessionManager;
+	}
+	
+	/**
+	 * @see UserRealm--->AuthorizingRealm
+	 * @return
+	 */
+	@Bean
+	@DependsOn(value="lifecycleBeanPostProcessor")
+	public UserRealm userRealm() {
+		UserRealm userRealm = new UserRealm();
+		userRealm.setCacheManager(cacheManager());
+		return userRealm;
+	}
+	
+	@Bean
+	public URLPermissionsFilter urlPermissionsFilter() {
+		return new URLPermissionsFilter();
+	}
+	
+	@Bean
+	public EhCacheManager cacheManager() {
+		EhCacheManager cacheManager = new EhCacheManager();
+		cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
+		return cacheManager;
+	}
+	
+	@Bean
+	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+		return new LifecycleBeanPostProcessor();
+	}
+}
