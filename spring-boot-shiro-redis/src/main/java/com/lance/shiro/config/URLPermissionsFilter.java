4ed8ce40f3bfe5b1eb512af664ee8307a5b00a81
lance <li6151770@126.com>
2016-06-30 09:15:36 +0800

    add url filter
    add url filter

---
+++ spring-boot-shiro-redis/src/main/java/com/lance/shiro/config/URLPermissionsFilter.java
@@ -0,0 +1,53 @@
+package com.lance.shiro.config;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.PermissionsAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lance.shiro.service.UserService;
+
+@Component("urlPermissionsFilter")
+public class URLPermissionsFilter extends PermissionsAuthorizationFilter{
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
+		String curUrl = getRequestUrl(request);
+		Subject subject = SecurityUtils.getSubject();
+		if(subject.getPrincipal() == null 
+				|| StringUtils.endsWithAny(curUrl, ".js",".css",".html")
+				|| StringUtils.endsWithAny(curUrl, ".jpg",".png",".gif", ".jpeg")
+				|| StringUtils.equals(curUrl, "/unauthor")) {
+			return true;
+		}
+		List<String> urls = userService.findPermissionUrl(subject.getPrincipal().toString());
+		
+		return urls.contains(curUrl);
+	}
+	
+	/**
+	 * 获取当前URL+Parameter
+	 * @author lance
+	 * @since  2014年12月18日 下午3:09:26
+	 * @param request	拦截请求request
+	 * @return			返回完整URL
+	 */
+	private String getRequestUrl(ServletRequest request) {
+		HttpServletRequest req = (HttpServletRequest)request;
+		String queryString = req.getQueryString();
+
+		queryString = StringUtils.isBlank(queryString)?"": "?"+queryString;
+		return req.getRequestURI()+queryString;
+	}
+}
