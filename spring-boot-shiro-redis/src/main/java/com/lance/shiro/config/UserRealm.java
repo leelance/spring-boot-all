ebdd5f3b3e66d304e4521fa69414909dd0c17f3e
lance <li6151770@126.com>
2016-06-30 09:15:47 +0800

    add user realm
    add user realm

---
+++ spring-boot-shiro-redis/src/main/java/com/lance/shiro/config/UserRealm.java
@@ -0,0 +1,61 @@
+package com.lance.shiro.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lance.shiro.domain.UserInfo;
+import com.lance.shiro.service.UserService;
+
+/**
+ * 验证用户登录
+ * 
+ * @author Administrator
+ */
+@Component("userRealm")
+public class UserRealm extends AuthorizingRealm {
+	@Autowired
+	private UserService userService;
+
+	public UserRealm() {
+		setName("UserRealm");
+		// 采用MD5加密
+		setCredentialsMatcher(new HashedCredentialsMatcher("md5"));
+	}
+
+	//权限资源角色
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		String username = (String) principals.getPrimaryPrincipal();
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+		//add Permission Resources
+		info.setStringPermissions(userService.findPermissions(username));
+		//add Roles String[Set<String> roles]
+		//info.setRoles(roles);
+		return info;
+	}
+	
+	//登录验证
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		UsernamePasswordToken upt = (UsernamePasswordToken) token;
+		String userName = upt.getUsername();
+		UserInfo user = userService.findByAccount(userName);
+
+		if (user == null) {
+			throw new UnknownAccountException();
+		}
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(userName, user.getPassword(), getName());
+		return info;
+	}
+}
