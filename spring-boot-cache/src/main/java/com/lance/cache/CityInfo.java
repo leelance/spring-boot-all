3b887930392d21ea491d1b75ab88ecdaaa09bef9
lance <li6151770@126.com>
2016-07-02 11:22:49 +0800

    add city info
    add city info

---
+++ spring-boot-cache/src/main/java/com/lance/cache/CityInfo.java
@@ -0,0 +1,32 @@
+package com.lance.cache;
+
+import java.io.Serializable;
+
+public class CityInfo implements Serializable{
+	private static final long serialVersionUID = 2845294956907027149L;
+	
+	private int id;
+	private String city;
+	
+	public CityInfo() {
+		
+	}
+	
+	public CityInfo(int id, String city) {
+		this.id = id;
+		this.city = city;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
