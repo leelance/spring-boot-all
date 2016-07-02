b46f981a6def18fc6bcc5398d3817091c637b8dd
lance <li6151770@126.com>
2016-07-02 10:52:05 +0800

    update cache suport
    update cache suport

--- spring-boot-cache/src/main/java/com/lance/cache/SimpleApplication.java
+++ spring-boot-cache/src/main/java/com/lance/cache/SimpleApplication.java
@@ -2,7 +2,11 @@ package com.lance.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class SimpleApplication {
 
