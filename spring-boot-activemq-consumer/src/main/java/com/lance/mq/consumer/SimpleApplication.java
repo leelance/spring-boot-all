fbc5264e298973a5b27ec7ec9972604edb11f0ec
lance <li6151770@126.com>
2016-07-04 10:53:07 +0800

    update activemq consumer support cache
    update activemq consumer support cache

--- spring-boot-activemq-consumer/src/main/java/com/lance/mq/consumer/SimpleApplication.java
+++ spring-boot-activemq-consumer/src/main/java/com/lance/mq/consumer/SimpleApplication.java
@@ -1,14 +1,24 @@
 package com.lance.mq.consumer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
 public class SimpleApplication {
+	static Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleApplication.class, args);
+		ConfigurableApplicationContext content = SpringApplication.run(SimpleApplication.class, args);
+		
+		//Test Bean
+		String[]beanNames = content.getBeanDefinitionNames();
+		for(String name: beanNames) {
+			logger.info("beanName: {}", name);
+		}
 	}
 }
--- spring-boot-activemq-consumer/src/main/java/com/lance/mq/consumer/config/ActiveMQConfig.java
+++ spring-boot-activemq-consumer/src/main/java/com/lance/mq/consumer/config/ActiveMQConfig.java
@@ -7,6 +7,7 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.jms.listener.MessageListenerContainer;
 import org.springframework.jms.listener.adapter.MessageListenerAdapter;
@@ -30,12 +31,18 @@ public class ActiveMQConfig {
 		return adapter;
 	}
 	
+	public CachingConnectionFactory connectionFactory(ActiveMQConnectionFactory connectionFactory) {
+		CachingConnectionFactory factory = new CachingConnectionFactory(connectionFactory);
+		return factory;
+	}
+	
 	@Bean
 	public MessageListenerContainer messageListenerContainer(ActiveMQConnectionFactory connectionFactory) {
 		DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
+		container.setConnectionFactory(connectionFactory(connectionFactory));
 		container.setDestination(helloQueue());
 		container.setMessageListener(messageListenerAdapter());
+		container.setConcurrency("10-50");
 		return container;
 	}
 	
