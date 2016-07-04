291c7810f18d5a72ac8958a47d482a89b95df6f3
lance <li6151770@126.com>
2016-07-04 10:56:41 +0800

    update activemq producer support pool
    update activemq producer support pool

--- spring-boot-activemq-producer/src/main/java/com/lance/mq/producer/config/ActiveMQConfig.java
+++ spring-boot-activemq-producer/src/main/java/com/lance/mq/producer/config/ActiveMQConfig.java
@@ -4,6 +4,7 @@ import javax.jms.Queue;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
@@ -20,9 +21,15 @@ public class ActiveMQConfig {
 		return new ActiveMQQueue(QUEUE_HELLO);
 	}
 	
+	public PooledConnectionFactory connectionFactory(ActiveMQConnectionFactory connectionFactory) {
+		PooledConnectionFactory pool = new PooledConnectionFactory(connectionFactory);
+		pool.setMaxConnections(5);
+		return pool;
+	}
+	
 	@Bean(name="jmsTemplate")
-	public JmsTemplate jmsTemplate(ActiveMQConnectionFactory connectionFactory ) {
-		JmsTemplate template = new JmsTemplate(connectionFactory);
+	public JmsTemplate jmsTemplate(ActiveMQConnectionFactory connectionFactory) {
+		JmsTemplate template = new JmsTemplate(connectionFactory(connectionFactory));
 		template.setDefaultDestination(helloQueue());
 		template.setMessageConverter(new SimpleMessageConverter());
 		return template;
