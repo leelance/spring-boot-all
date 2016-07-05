7cef806f5f5e351643b28ae910927f27bb10c868
lance <li6151770@126.com>
2016-07-05 14:47:36 +0800

    add resources

---
+++ spring-boot-freemarker/src/main/java/com/lance/freemaker/SimpleApplication.java
@@ -0,0 +1,14 @@
+package com.lance.freemaker;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.lance.freemaker.mapper")
+public class SimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApplication.class, args);
+	}
+}
