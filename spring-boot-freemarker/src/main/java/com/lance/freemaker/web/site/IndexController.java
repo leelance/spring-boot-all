7d53c087fdef0cd2e3fd005d191b3ee6df952fe4
lance <li6151770@126.com>
2016-07-06 00:37:37 +0800

    add resource

---
+++ spring-boot-freemarker/src/main/java/com/lance/freemaker/web/site/IndexController.java
@@ -0,0 +1,13 @@
+package com.lance.freemaker.web.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value={"","/","index"})
+	public String index() {
+		return "site/index.jsp";
+	}
+}
