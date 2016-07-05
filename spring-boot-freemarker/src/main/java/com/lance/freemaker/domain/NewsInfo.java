4b4a2f6302755473ccc29890adc822d37b3878d6
lance <li6151770@126.com>
2016-07-06 00:36:29 +0800

    add resource
    add resource

---
+++ spring-boot-freemarker/src/main/java/com/lance/freemaker/domain/NewsInfo.java
@@ -0,0 +1,66 @@
+package com.lance.freemaker.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class NewsInfo implements Serializable{
+	private static final long serialVersionUID = -6021031812038355073L;
+	
+	private int id;
+	private String title;
+	private String summary;
+	private String author;
+	private String content;
+	private int status = 1;
+	private Date createTime;
+	private Date updateTime;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+}
