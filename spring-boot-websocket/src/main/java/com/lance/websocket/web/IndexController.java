9b40ed11b441e243344d09c25eb05f662d9b9c95
lance <li6151770@126.com>
2016-07-13 15:56:28 +0800

    add index controller
    add index controller

---
+++ spring-boot-websocket/src/main/java/com/lance/websocket/web/IndexController.java
@@ -0,0 +1,27 @@
+package com.lance.websocket.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	/**
+	 * Index
+	 * @return
+	 */
+	@RequestMapping(value={"", "/", "index"})
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("chat")
+	public String chat() {
+		return "chat.jsp";
+	}
+	
+	@RequestMapping("game")
+	public String game() {
+		return "index.jsp";
+	}
+}
