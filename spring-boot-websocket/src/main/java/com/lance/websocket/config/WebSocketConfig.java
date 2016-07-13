ffefbea61703f847b8e5353da981f65205deb3da
lance <li6151770@126.com>
2016-07-13 16:56:34 +0800

    add websocket config

---
+++ spring-boot-websocket/src/main/java/com/lance/websocket/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.lance.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.lance.websocket.service.EchoService;
+import com.lance.websocket.service.EchoServiceImpl;
+
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer{
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoWebSocketHandler(), "/echo").withSockJS();
+	}
+	
+	@Bean
+	public WebSocketHandler echoWebSocketHandler() {
+		return new EchoWebSocketHandler(echoService());
+	}
+	
+	@Bean
+	public EchoService echoService() {
+		return new EchoServiceImpl("Did you say \"%s\"?");
+	}
+}
