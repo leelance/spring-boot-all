98578a4412b1d081a9874a14071789a15214a917
lance <li6151770@126.com>
2016-07-13 16:57:15 +0800

    update autoconfig websocket

--- spring-boot-websocket/src/main/java/com/lance/websocket/SimpleApplication.java
+++ spring-boot-websocket/src/main/java/com/lance/websocket/SimpleApplication.java
@@ -2,8 +2,10 @@ package com.lance.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class SimpleApplication {
 
 	public static void main(String[] args) {
