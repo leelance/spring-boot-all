00aa9d6dfd16d16c8b43046e3811e8aa40480499
lance <li6151770@126.com>
2016-08-19 15:15:09 +0800

    Set<SocketSessionInfo>

--- spring-boot-websocket/src/main/java/com/lance/websocket/config/SocketSessionHandler.java
+++ spring-boot-websocket/src/main/java/com/lance/websocket/config/SocketSessionHandler.java
@@ -1,13 +1,15 @@
 package com.lance.websocket.config;
 
-import java.util.Map;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.springframework.web.socket.WebSocketSession;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public final class SocketSessionHandler {
-	public static Map<String, WebSocketSession> sessions = Maps.newConcurrentMap();
+	public static Set<SocketSessionInfo> sessions = Sets.newConcurrentHashSet();
 	
 	private SocketSessionHandler(){}
 	
@@ -19,11 +21,45 @@ public final class SocketSessionHandler {
 		return SocketSessionHandlerHolder.handler;
 	}
 	
+	/**
+	 * Add WebSocketSession
+	 * @param session
+	 */
 	public synchronized void addSession(WebSocketSession session){
-        sessions.put(session.getId(), session);
+        sessions.add(findOne(session));
+    }
+	
+	/**
+	 * find WebSocketSession
+	 * @param session
+	 * @return
+	 */
+	public synchronized SocketSessionInfo findOne(WebSocketSession session){
+		SocketSessionInfo info = null;
+		Iterator<SocketSessionInfo> it = sessions.iterator();
+		while(it.hasNext()) {
+			if(it.next().getSessionId().equals(session.getId())){
+				info = it.next();
+				info.setLastSessionTime(new Date());
+			}
+		}
+		
+		if(info == null) {
+			info = new SocketSessionInfo();
+			info.setSessionId(session.getId());
+			info.setWebSocketSession(session);
+			info.setLastSessionTime(new Date());
+		}
+		return info;
     }
 
+	/**
+	 * remove WebSocketSession
+	 * @param id
+	 */
 	public synchronized void removeSession(String id) {
-        sessions.remove(id);
+		SocketSessionInfo info = new SocketSessionInfo();
+		info.setSessionId(id);
+		sessions.remove(info);
     }
 }
