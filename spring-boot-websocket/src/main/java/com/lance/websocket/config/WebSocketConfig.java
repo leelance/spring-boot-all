93ed9462ddba0899db7ee03a1281f0c89cd65aae
lance <li6151770@126.com>
2016-08-19 15:14:47 +0800

    update setTaskScheduler

--- spring-boot-websocket/src/main/java/com/lance/websocket/config/WebSocketConfig.java
+++ spring-boot-websocket/src/main/java/com/lance/websocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.lance.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -18,7 +19,8 @@ public class WebSocketConfig implements WebSocketConfigurer{
 				.setAllowedOrigins("*")
 				.withSockJS()
 				.setHeartbeatTime(30000)
-				.setSessionCookieNeeded(true);
+				.setSessionCookieNeeded(true)
+				.setTaskScheduler(taskScheduler());
 	}
 	
 	@Bean
@@ -29,5 +31,12 @@ public class WebSocketConfig implements WebSocketConfigurer{
 	@Bean
 	public EchoService echoService() {
 		return new EchoServiceImpl("Did you say \"%s\"?");
+	}
+	
+	@Bean
+	public ConcurrentTaskScheduler taskScheduler() {
+		ConcurrentTaskScheduler task = new ConcurrentTaskScheduler();
+		task.scheduleAtFixedRate(new SockTaskRunner(), 1000*60*SocketSessionInfo.SESSION_INVALID);
+		return task;
 	}
 }
