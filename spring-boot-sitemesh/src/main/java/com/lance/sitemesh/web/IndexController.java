7b509be0811cd16add588b841e081598f6165333
lance <li6151770@126.com>
2016-09-01 10:21:31 +0800

    add index.jsp

--- spring-boot-sitemesh/src/main/java/com/lance/sitemesh/web/IndexController.java
+++ spring-boot-sitemesh/src/main/java/com/lance/sitemesh/web/IndexController.java
@@ -1,5 +1,14 @@
 package com.lance.sitemesh.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/")
 public class IndexController {
 
+	@RequestMapping("index")
+	public String index(){
+		return "index.jsp";
+	}
 }
