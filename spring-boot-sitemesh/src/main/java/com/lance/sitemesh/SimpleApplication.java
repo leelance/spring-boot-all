6c7a0ad6c7e0357334e5b60dc70f2d2d00982fb3
lance <li6151770@126.com>
2016-09-01 10:21:17 +0800

    update simple

--- spring-boot-sitemesh/src/main/java/com/lance/sitemesh/SimpleApplication.java
+++ spring-boot-sitemesh/src/main/java/com/lance/sitemesh/SimpleApplication.java
@@ -1,11 +1,9 @@
-package com.lance.shiro;
+package com.lance.sitemesh;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lance.shiro.mapper")
 public class SimpleApplication {
 
 	public static void main(String[] args) {
