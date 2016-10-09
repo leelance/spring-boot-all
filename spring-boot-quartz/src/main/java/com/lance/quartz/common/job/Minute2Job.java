7d2feab8dc4df8352525318cd2572a953754016c
lance <li6151770@126.com>
2016-10-09 15:21:13 +0800

    add job test2

---
+++ spring-boot-quartz/src/main/java/com/lance/quartz/common/job/Minute2Job.java
@@ -0,0 +1,17 @@
+package com.lance.quartz.common.job;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class Minute2Job implements Job {
+	Logger logger = LogManager.getLogger(getClass());
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		logger.info("JobName2: {}", context.getJobDetail().getKey().getName());
+	}
+
+}
