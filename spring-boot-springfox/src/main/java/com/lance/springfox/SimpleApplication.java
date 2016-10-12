a6af0f47af056c8dc7335c4e39730b16f1a1de3e
lance <li6151770@126.com>
2016-10-12 13:17:08 +0800

    add springboot run application

---
+++ spring-boot-springfox/src/main/java/com/lance/springfox/SimpleApplication.java
@@ -0,0 +1,12 @@
+package com.lance.springfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApplication.class, args);
+	}
+}
