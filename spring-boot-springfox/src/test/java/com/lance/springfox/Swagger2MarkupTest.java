518be5ce9ce7f5c38ded6bdb899298c6db9da57e
lance <li6151770@126.com>
2016-10-12 13:17:40 +0800

    add junit test swagger2Markuptest

---
+++ spring-boot-springfox/src/test/java/com/lance/springfox/Swagger2MarkupTest.java
@@ -0,0 +1,78 @@
+package com.lance.springfox;
+
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.lance.springfox.common.SwaggerConfig;
+
+import io.github.robwin.markup.builder.MarkupLanguage;
+import springfox.documentation.staticdocs.Swagger2MarkupResultHandler;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes={SimpleApplication.class, SwaggerConfig.class})
+public class Swagger2MarkupTest {
+	@Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    @Ignore
+    public void convertSwaggerToAsciiDoc() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs?group=Admin API")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(Swagger2MarkupResultHandler.outputDirectory("src/docs/asciidoc/generated").build())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Ignore
+    public void convertSwaggerToMarkdown() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs?group=Admin API")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(Swagger2MarkupResultHandler.outputDirectory("src/docs/markdown/generated")
+                    .withMarkupLanguage(MarkupLanguage.MARKDOWN).build())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    
+    @Test
+    @Ignore
+    public void createSpringfoxSwaggerJson() throws Exception {
+        String outputDir = "E:\\gitwork\\spring-boot-all\\spring-boot-springfox\\src\\docs\\";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs?group=Admin API")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String swaggerJson = response.getContentAsString();
+        Files.createDirectories(Paths.get(outputDir));
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputDir, "swagger.json"), StandardCharsets.UTF_8)){
+            writer.write(swaggerJson);
+        }
+    }
+}
