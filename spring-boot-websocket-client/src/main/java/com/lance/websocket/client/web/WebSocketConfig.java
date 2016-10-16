72fba455c8d0ad115622adb664fa215c77bf8b1b
lance <li6151770@126.com>
2016-10-16 13:42:25 +0800

    update websocket config

---
+++ spring-boot-websocket-client/src/main/java/com/lance/websocket/client/web/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.lance.websocket.client.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws_notice").withSockJS();
+	}
+}
