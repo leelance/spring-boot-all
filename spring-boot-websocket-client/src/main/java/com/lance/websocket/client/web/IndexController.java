188d780ec42e5d268f1e318abdef3c7db2277014
lance <li6151770@126.com>
2016-10-16 13:42:02 +0800

    add requestmap method

--- spring-boot-websocket-client/src/main/java/com/lance/websocket/client/web/IndexController.java
+++ spring-boot-websocket-client/src/main/java/com/lance/websocket/client/web/IndexController.java
@@ -1,10 +1,19 @@
 package com.lance.websocket.client.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
 
 	/**
 	 * Index
@@ -20,8 +29,29 @@ public class IndexController {
 		return "chat.jsp";
 	}
 	
-	@RequestMapping("game")
-	public String game() {
-		return "index.jsp";
+	@RequestMapping("notifications")
+	public String notifications() {
+		return "notifications.jsp";
+	}
+	
+	@RequestMapping("notifications-button")
+	public String notificationsButton() {
+		return "notifications-buttion.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
+	public void sendMessage() {
+		List<MessageInfo> messages = getMessage();
+		messagingTemplate.convertAndSend("/user/topic/message", messages);
+	}
+	
+	private List<MessageInfo> getMessage(){
+		List<MessageInfo> list = new ArrayList<>();
+		list.add(new MessageInfo("Message-Text1"));
+		list.add(new MessageInfo("Message-Text2"));
+		list.add(new MessageInfo("Message-Text3"));
+		list.add(new MessageInfo("Message-Text4"));
+		return list;
 	}
 }
