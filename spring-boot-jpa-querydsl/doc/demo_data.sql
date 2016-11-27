7a2ca3f27254e7a8dad21f9728701d503d4f1346
lance <li6151770@126.com>
2016-11-27 10:39:15 +0800

    add querydsl init data

---
+++ spring-boot-jpa-querydsl/doc/demo_data.sql
@@ -0,0 +1,22 @@
+-- ----------------------------
+-- Records of t_city
+-- ----------------------------
+INSERT INTO `t_city` VALUES ('2', 'China', 'China.ShangHai', 'ShangHai', '1');
+INSERT INTO `t_city` VALUES ('3', 'China', 'China.NanJing', 'NanJing', '1');
+INSERT INTO `t_city` VALUES ('4', 'China', 'China.BeiJing', 'BeiJing', '1');
+INSERT INTO `t_city` VALUES ('5', 'China', 'China.GuangDong', 'GuangDong', '1');
+INSERT INTO `t_city` VALUES ('6', 'China', 'China.ShanDong', 'ShangDong', '0');
+-- ----------------------------
+-- Records of t_hotel
+-- ----------------------------
+INSERT INTO `t_hotel` VALUES ('1', 'ShangHai.HutaiLu', 'Seven.Day', '2');
+INSERT INTO `t_hotel` VALUES ('2', 'ShangHai.HongQiao', 'MoTai', '2');
+INSERT INTO `t_hotel` VALUES ('3', 'ShangHai.JiangXi', 'HanTing', '2');
+INSERT INTO `t_hotel` VALUES ('4', 'ShangHai.RenMin', 'Eight.Day', '2');
+INSERT INTO `t_hotel` VALUES ('5', 'ShangHai', 'Nine.Day', '2');
+INSERT INTO `t_hotel` VALUES ('8', 'BeiJing', 'Seven.Day', '3');
+INSERT INTO `t_hotel` VALUES ('9', 'BeiJing', 'Nine.Day', '3');
+INSERT INTO `t_hotel` VALUES ('10', 'NanJing', 'MoTai', '4');
+INSERT INTO `t_hotel` VALUES ('11', 'NanJing', 'HanTing', '4');
+INSERT INTO `t_hotel` VALUES ('12', 'GuangDong', 'Nine.Day', '5');
+INSERT INTO `t_hotel` VALUES ('13', 'GuangDong', 'Eight.Day', '5');
