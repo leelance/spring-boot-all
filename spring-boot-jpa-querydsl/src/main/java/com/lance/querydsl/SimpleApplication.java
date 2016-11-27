42602d4b37d0daec1dc43880ad2179cc9d72a441
lance <li6151770@126.com>
2016-11-27 10:41:14 +0800

    add simple application main

---
+++ spring-boot-jpa-querydsl/src/main/java/com/lance/querydsl/SimpleApplication.java
@@ -0,0 +1,19 @@
+package com.lance.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SimpleApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SimpleApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApplication.class, args);
+	}
+}
