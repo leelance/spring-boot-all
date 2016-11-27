ed423908a0466ddaebac172efe4ffb18eac13ddb
lance <li6151770@126.com>
2016-11-27 10:40:24 +0800

    add controller return result

---
+++ spring-boot-jpa-querydsl/src/main/java/com/lance/querydsl/json/ResponseBodyInfo.java
@@ -0,0 +1,54 @@
+package com.lance.querydsl.json;
+
+public class ResponseBodyInfo<T> {
+	// 错误代码
+	private int errorCode;
+	// 错误提示
+	private String errorText;
+
+	// 返回对象
+	private T data;
+
+	protected ResponseBodyInfo() {
+	}
+
+	protected ResponseBodyInfo(int errorCode, String errorText, T data) {
+		this.errorCode = errorCode;
+		this.errorText = errorText;
+		this.data = data;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorText() {
+		return errorText;
+	}
+
+	public void setErrorText(String errorText) {
+		this.errorText = errorText;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("ResponseBodyInfo{");
+		sb.append("errorCode=").append(errorCode);
+		sb.append(", errorText='").append(errorText).append('\'');
+		sb.append(", data=").append(data);
+		sb.append('}');
+		return sb.toString();
+	}
+}
