1b9260a45c6564ffd205baa9e8edfa8b9a5b5494
lance <li6151770@126.com>
2016-11-27 10:40:39 +0800

    add city hotel repository

---
+++ spring-boot-jpa-querydsl/src/main/java/com/lance/querydsl/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.lance.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lance.querydsl.entity.CityEntity;
+
+public interface CityRepository extends JpaRepository<CityEntity, Long>{
+
+}
---
+++ spring-boot-jpa-querydsl/src/main/java/com/lance/querydsl/repository/HotelRepository.java
@@ -0,0 +1,9 @@
+package com.lance.querydsl.repository;
+
+import org.springframework.data.repository.Repository;
+
+import com.lance.querydsl.entity.HotelEntity;
+
+public interface HotelRepository extends Repository<HotelEntity, Long>{
+
+}
