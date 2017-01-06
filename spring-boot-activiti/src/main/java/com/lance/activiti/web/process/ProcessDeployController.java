41748f7be8a08a0c7d38fa9e10391e8fc956024f
lance <li6151770@126.com>
2017-01-06 16:25:19 +0800

    add process deply controller

---
+++ spring-boot-activiti/src/main/java/com/lance/activiti/web/process/ProcessDeployController.java
@@ -0,0 +1,10 @@
+package com.lance.activiti.web.process;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/process/deploy/")
+public class ProcessDeployController {
+
+}
