d952c5badd2f621c77dffe8c79c423857e403760
lance <lh160@zcsmart.com>
2019-03-01 22:41:47 +0800

    update spring-boot 1.5.19
    update spring-boot-starter version
    1.5.19.RELEASE

---
+++ spring-boot-sharding/src/main/java/com/lance/sharding/SpringBootShardingApplication.java
@@ -0,0 +1,13 @@
+package com.lance.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShardingApplication.class, args);
+    }
+
+}
---
+++ spring-boot-sharding/src/test/java/com/lance/sharding/SpringBootShardingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lance.sharding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootShardingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
